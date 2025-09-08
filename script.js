2170b00383ddceb18a886979c461a25742059c1f
LarcherDev <larcherdev@gmail.com>
2025-09-07 23:31:54 -0300

    correção de credenciais

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function login() {
-const email = document.getElementById("email").value;
-const senha = document.getElementById("senha").value;
-const retorno = document.getElementById("retorno");
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const retorno = document.getElementById("retorno");
+  const SalvarLogin = document.getElementById("salvarLogin").checked;
 
-retorno.innerHTML = `Email: ${email}<br>Senha: ${senha}`;
+  retorno.innerHTML = `Email: ${email}<br>Senha: ${senha}<br>Salvar Login: ${SalvarLogin}`;
+}
+
+function validarEmail() {
+  const emailInput = document.getElementById("email");
+  const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|com\.br)$/;
+  const retorno = document.getElementById("retorno");
+  if (!pattern.test(emailInput.value)) {
+    retorno.textContent = "E-mail inválido!";
+    retorno.style.color = "red";
+    return false;
+  }
+  retorno.textContent = "";
+  login();
+  retorno.style.color = "green";
+  return false;
 }
